ffa527203ab2e8bc6c9df6beac3a9f6fbff5a810
Zuhal13 <89143761+Zuhal13@users.noreply.github.com>
2022-01-22 00:41:35 +0300

    Add files via upload

---
+++ Assets/Scripts/SwerveMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwerveMovement : MonoBehaviour
+{
+    private SwerveInputSystem swerveInputSystem;
+    [SerializeField] private float swerveSpeed = 0.5f;
+
+    private void Awake()
+    {
+        swerveInputSystem = GetComponent<SwerveInputSystem>();
+
+
+    }
+    private void Update()
+    {
+        float swerveAmount = Time.deltaTime * swerveSpeed * swerveInputSystem.MoveFactorX;
+        transform.Translate(swerveAmount, 0, 0);
+    }
+}
